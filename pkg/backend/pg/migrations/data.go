0f0f21b5d4952ed9f7fdaef5c14960b1d3110b42
Jeff Martin <jeffmartin@gmail.com>
2021-11-06 11:05:47 -0700

    chore: rebuild protos

--- pkg/backend/pg/migrations/data.go
+++ pkg/backend/pg/migrations/data.go
@@ -83,7 +83,7 @@ func _0001_createUpSql() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "0001_create.up.sql", size: 2736, mode: os.FileMode(436), modTime: time.Unix(1627951284, 0)}
+	info := bindataFileInfo{name: "0001_create.up.sql", size: 2736, mode: os.FileMode(436), modTime: time.Unix(1632474072, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
